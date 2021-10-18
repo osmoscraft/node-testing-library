c822c60e51734562563ce2ee79f80dc87814a0ec
sora <chuanqi.sun.dev@gmail.com>
2021-10-17 18:08:30 -0700

    refactor

--- src/assertion.ts
+++ src/assertion.ts
@@ -5,82 +5,119 @@ interface ExpectContext {
   negated: boolean;
 }
 
-export function expect(actual: any) {
-  const expectContext: ExpectContext = {
-    actual,
-    negated: false,
-  };
+export class Expect {
+  static create(actual: any) {
+    return new Expect(actual);
+  }
 
-  return {
-    get not() {
-      expectContext.negated = !expectContext.negated;
-      return this;
-    },
-    toThrow: chainToThrow.bind(expectContext),
-    toEqual: chainToEqual.bind(expectContext),
-  };
-}
+  private context: ExpectContext;
 
-async function chainToThrow(this: ExpectContext, expectedError?: any) {
-  try {
-    await this.actual();
-    throw new _InternalFallthroughError();
-  } catch (actualError) {
-    const isThrown = !(actualError instanceof _InternalFallthroughError);
-    const isExpectedToThrow = !this.negated;
-    const isErrorTypeSpecificed = expectedError !== undefined;
-    const isExpectedErrorType = expectedError && actualError instanceof expectedError;
+  constructor(actual: any) {
+    this.context = {
+      actual,
+      negated: false,
+    };
+  }
 
-    if (isExpectedToThrow && isThrown && isErrorTypeSpecificed && isExpectedErrorType) {
-      return;
-    } else if (isExpectedToThrow && isThrown && isErrorTypeSpecificed && !isExpectedErrorType) {
-      throw new WrongErrorThrow(expectedError, actualError);
-    } else if (isExpectedToThrow && isThrown && !isErrorTypeSpecificed) {
-      return;
-    } else if (isExpectedToThrow && !isThrown) {
-      throw new ExpectedErrorNotThrown(expectedError);
-    } else if (!isExpectedToThrow && isThrown && isErrorTypeSpecificed && isExpectedErrorType) {
-      throw new UnexpectedThrown(actualError);
-    } else if (!isExpectedToThrow && isThrown && isErrorTypeSpecificed && !isExpectedErrorType) {
-      return;
-    } else if (!isExpectedToThrow && isThrown && !isErrorTypeSpecificed) {
-      throw new UnexpectedThrown(actualError);
-    } else if (!isExpectedToThrow && !isThrown) {
-      return;
+  get not() {
+    this.context.negated = !this.context.negated;
+    return this;
+  }
+
+  async toThrow(expectedError?: any) {
+    try {
+      await this.context.actual();
+      throw new _InternalFallthroughError();
+    } catch (actualError) {
+      const isThrown = !(actualError instanceof _InternalFallthroughError);
+      const isExpectedToThrow = !this.context.negated;
+      const isErrorTypeSpecificed = expectedError !== undefined;
+      const isExpectedErrorType =
+        expectedError && actualError instanceof expectedError;
+
+      if (
+        isExpectedToThrow &&
+        isThrown &&
+        isErrorTypeSpecificed &&
+        isExpectedErrorType
+      ) {
+        return;
+      } else if (
+        isExpectedToThrow &&
+        isThrown &&
+        isErrorTypeSpecificed &&
+        !isExpectedErrorType
+      ) {
+        throw new WrongErrorThrow(expectedError, actualError);
+      } else if (isExpectedToThrow && isThrown && !isErrorTypeSpecificed) {
+        return;
+      } else if (isExpectedToThrow && !isThrown) {
+        throw new ExpectedErrorNotThrown(expectedError);
+      } else if (
+        !isExpectedToThrow &&
+        isThrown &&
+        isErrorTypeSpecificed &&
+        isExpectedErrorType
+      ) {
+        throw new UnexpectedThrown(actualError);
+      } else if (
+        !isExpectedToThrow &&
+        isThrown &&
+        isErrorTypeSpecificed &&
+        !isExpectedErrorType
+      ) {
+        return;
+      } else if (!isExpectedToThrow && isThrown && !isErrorTypeSpecificed) {
+        throw new UnexpectedThrown(actualError);
+      } else if (!isExpectedToThrow && !isThrown) {
+        return;
+      }
+
+      throw new Error("Test framework error. Unreachable code is executed");
     }
+  }
 
-    throw new Error("Test framework error. Unreachable code is executed");
+  async toEqual(expectedValue: any) {
+    try {
+      assert.deepEqual(this.context.actual, expectedValue);
+      throw new _InternalFallthroughError();
+    } catch (actualError) {
+      const isEqual = actualError instanceof _InternalFallthroughError;
+      const isEqualExpected = !this.context.negated;
+
+      if (isEqual && isEqualExpected) return;
+      if (!isEqual && !isEqualExpected) return;
+      if (isEqual && !isEqualExpected)
+        throw new UnexpectedEquality(
+          JSON.stringify(expectedValue),
+          JSON.stringify(this.context.actual)
+        );
+      if (!isEqual && isEqualExpected)
+        throw new UnexpectedInequality(
+          JSON.stringify(expectedValue),
+          JSON.stringify(this.context.actual)
+        );
+    }
   }
 }
 
-async function chainToEqual(this: ExpectContext, expectedValue: any) {
-  try {
-    assert.deepEqual(this.actual, expectedValue);
-    throw new _InternalFallthroughError();
-  } catch (actualError) {
-    const isEqual = actualError instanceof _InternalFallthroughError;
-    const isEqualExpected = !this.negated;
-
-    if (isEqual && isEqualExpected) return;
-    if (!isEqual && !isEqualExpected) return;
-    if (isEqual && !isEqualExpected)
-      throw new UnexpectedEquality(JSON.stringify(expectedValue), JSON.stringify(this.actual));
-    if (!isEqual && isEqualExpected)
-      throw new UnexpectedInequality(JSON.stringify(expectedValue), JSON.stringify(this.actual));
-  }
-}
+export const expect = Expect.create;
 
 class ExpectedErrorNotThrown extends Error {
   constructor(expected: any) {
     super();
-    this.message = `"${expected?.name ?? "(Unnamed error)"}" was expected to throw, but no error was thrown.`;
+    this.message = `"${
+      expected?.name ?? "(Unnamed error)"
+    }" was expected to throw, but no error was thrown.`;
   }
 }
 
 class WrongErrorThrow extends Error {
   constructor(expected: any, actual: any) {
     super();
-    this.message = `${expected?.name ?? "(Unnamed error)"}" was expected to be thrown, but "${
+    this.message = `${
+      expected?.name ?? "(Unnamed error)"
+    }" was expected to be thrown, but "${
       actual?.name ?? "(unnamed error)"
     }" was thrown.`;
   }
@@ -89,7 +126,9 @@ class WrongErrorThrow extends Error {
 class UnexpectedThrown extends Error {
   constructor(actual: any) {
     super();
-    this.message = `"${actual?.name ?? "(Unnamed error)"}" was unexpectedly thrown.`;
+    this.message = `"${
+      actual?.name ?? "(Unnamed error)"
+    }" was unexpectedly thrown.`;
   }
 }
 
